2d454ade0509ffcb3538bf11bdfdce0b0120ba4c
rebzhaki <rebzhaki@gmail.com>
2023-02-22 17:09:13 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>{/* <Route path="/:from" element={<Home />} /> */}</Routes>
+    </div>
+  );
+}
+
+export default App;
